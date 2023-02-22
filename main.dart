c0c4c8c158fa6de53cda85e4e3a2d73d2dcdce6b
keerthanaasri <125664317+keerthanaasri@users.noreply.github.com>
2023-02-22 17:36:46 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,117 @@
+//using libabies
+import 'dart:math' as math;
+void main(){
+
+//declare variable
+int x=4;
+int y=5;
+
+//addition program
+int add=x+y;
+print('addition=$add');
+
+//subtraction
+int sub=y-x;
+print('subtraction=$sub');
+
+//multiplication
+int mul=x*y;
+print('multiplication=$mul');
+
+//division
+double div=x/y;
+print('division=$div');
+
+//modulus
+int mod=x%y;
+print('modulus=$mod');
+
+//floor division
+int flo=x~/y;
+print('floor division=$flo');
+
+//operator precedence
+var value=2+(1+1-2)*5;
+print('operator precedence=$value');
+
+//area of circle
+var radius=15;
+var area;
+area=3.14*radius*radius;
+print('area of circle=$area');
+
+//SQUARE
+var SQ;
+SQ=math.pow(x,2);
+print('sq=$SQ');
+
+//cube
+var cube;
+cube=math.pow(y,3);
+print('cube=$cube');
+
+//exponent
+var exp;
+exp=math.pow(x,y);
+print('exponent=$exp');
+
+//square root
+var root=math.sqrt(x);
+print('square root=$root');
+
+//constant
+print('value of PI:math.pi');
+
+//trigonomentry
+double degree=90;
+var cos=math.cos(degree);
+print('trigonomentry=$cos');
+
+var radian=degree*(math.pi/90);
+cos=math.cos(radian);
+print('cosvalue=$cos');
+
+//parallelogram
+var para;
+var b=54;
+var h=64.5;
+para=b*h;
+print('area of parallelogram=$para');
+
+//trapezoid
+var tra;
+var a=3,bre=6;
+var height=34.5;
+tra=a+bre/2*height;
+print('area of trapezoid=$tra');
+
+//square
+var SQUARE;
+var Area=53;
+SQUARE=Area*Area;
+print('Area of SQUARE=$SQUARE');
+
+//triangle
+var tri,base,HEIGHT;
+base=56;
+HEIGHT=67;
+tri=HEIGHT*base/2;
+print('area of triangle=$tri');
+
+//pentagon
+var pentagon,p,AREA;
+AREA=56;
+p=67;
+pentagon=1/2 * (p*AREA);
+print('area of pentagon=$pentagon');
+
+//price of the circle
+var total_land=270;
+var radius=14;
+var total_price=44;
+area of_circle= 3.14*radius*radius;
+print('area of cicrle=$price of circle');
+
+//with math libabies
+// area of_circle=pi*radius*
+}
